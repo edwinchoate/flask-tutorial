0e81fd28f805d5017d42f6c219c8effe8635c663
Edwin Choate <edwin.choate@pm.me>
2025-08-08 18:17:26 -0400

    adds db.py

---
+++ flaskr/db.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import g, current_app
+
+# g is a shared object that allows you to share and re-use data (such as a db connection) across requests
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        # sqlite3.Row means return rows that behave like dicts. This allows accessing columns by name.
+        g.db.row_factory = sqlite3.Row 
+    return g.db
+
+
+def close_db():
+    db = g.pop('db', None)
+    if g.db is not None:
+        db.close()
