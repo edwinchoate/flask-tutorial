486482bb11f53b81c76b62b8048764140506f83e
Edwin Choate <edwin.choate@pm.me>
2025-08-08 11:26:04 -0400

    adds config.py

---
+++ instance/config.py
@@ -0,0 +1,5 @@
+# Normally this file shouldn't be checked into git, but this project is an exception because 
+# I'm documenting what I've done in this tutorial
+import os 
+
+SECRET_KEY = 'ThisIsATutorialProject' # For generating real ones, see secrets.token_hex()
